d1d08ecf780e37fd402bcc93b03205df00d8297f
LeeGeumLak <ekdma4105@naver.com>
2020-06-24 23:24:24 +0900

    방송하기 페이지 기본 레이아웃만 씌움

--- root/broadcast.php
+++ root/broadcast.php
@@ -1,9 +1,31 @@
+<?php
+
+?>
+
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+        <?php include './header.php'?>
+    </head>
 
     <body>
-        <p><?php echo "방송 화면 넣을 예정"; ?></p>
+    <!--  최상단 네비게이션바     -->
+    <?php include './topPart.php'?>
 
+    <!--  방송 화면을 넣을 예정  -->
+    <div class="section-container">
+        <div class="about-area" id="about">
+            <div class="title">
+                BROADCAST
+            </div>
+
+
+        </div>
+    </div>
+
+    <?php include './footer.php'?>
+
+
+    <script src="../js/myPortfolioWeb.js"></script>
     </body>
 </html>
