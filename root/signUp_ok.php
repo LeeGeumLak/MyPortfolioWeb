5932c4c79f70cab1654874f09bb9c8a36d715dbb
LeeGeumLak <ekdma4105@naver.com>
2020-06-25 11:43:45 +0900

    회원가입 실패 요인 찾는중

--- root/signUp_ok.php
+++ root/signUp_ok.php
@@ -10,15 +10,15 @@
     // auto_increment 값 초기화
     $sql_InitIncrement = mq("alter table userInfo auto_increment =1");
 
-    $sql = mq("insert into userInfo (userId, nickName, userPassword, createDate, loginDate) values ('.$userId', '.$nickName', '.$userPassword', now(), now())");
-
-    if ($sql == TRUE) {
+    if($userId && $nickName && $userPassword) {
+        $sql = mq("insert into userInfo (userId, nickName, userPassword, createDate, loginDate) values ('".$userId."', '".$nickName."', '".$userPassword."', now(), now())");
         echo "<script>alert('회원가입이 완료됐습니다.');</script>";
-    } else {
+    }
+    else {
         echo "<script>alert('회원가입에 실패했습니다.');</script>";
+        echo "<script>location.replace('./signIn.php');</script>";
     }
 
-    echo("<script>location.replace('./signIn.php');</script>");
 ?>
 
 
