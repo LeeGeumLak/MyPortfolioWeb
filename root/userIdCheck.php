af7c518ea4123bdaaf04bfa65d7d5eae303a1c37
LeeGeumLak <ekdma4105@naver.com>
2020-06-25 11:09:46 +0900

    아이디 유효성 검사 오타 수정

--- root/userIdCheck.php
+++ root/userIdCheck.php
@@ -4,7 +4,7 @@
     //Post로 받은 데이터 가져오기
     $userId = $_POST['userId'];
 
-    $sql = "SELECT COUNT(*) FROM user WHERE userID = '{$userId}'";
+    $sql = "SELECT COUNT(*) FROM user WHERE userId = '{$userId}'";
     $result = $db->query($sql);
     $row = $result->fetch_assoc();
     echo(json_encode(array("result" => $row["COUNT(*)"])));
