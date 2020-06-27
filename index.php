b42f23ad05e8e261db4b5cfdb28a05574367161a
LeeGeumLak <ekdma4105@naver.com>
2020-06-27 13:56:56 +0900

    <meta> 태그 위치 변경

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
-<meta name="google-site-verification" content="avUJ1EXVTYeClRcjbyA9wQy3jAghkCv-ILIzAYtIQKo" />
 <!doctype html>
     <head>
         <?php include './root/header.php' ?>
+        <meta name="google-site-verification" content="avUJ1EXVTYeClRcjbyA9wQy3jAghkCv-ILIzAYtIQKo" />
     </head>
 
     <body>
