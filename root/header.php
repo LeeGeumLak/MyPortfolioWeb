541e8b0523ed907da0bcf7d451f8caa2f997d403
LeeGeumLak <ekdma4105@naver.com>
2020-06-29 16:18:05 +0900

    게시글 및 댓글 폰트 사이즈, 위치 수정

--- root/header.php
+++ root/header.php
@@ -13,6 +13,8 @@
 <!-- style css 파일 가져오기 -->
 <link rel="stylesheet" href="../css/style.css" />
 
+<script src="../js/myPortfolioWeb.js"></script>
+
 <!-- Bootstrap cdn 설정 (영어버전) -->
 <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">-->
 
