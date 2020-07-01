8c2cddb3cbf2fc1eebc6c5c9f8b3f7017f12042d
LeeGeumLak <ekdma4105@naver.com>
2020-07-01 14:51:09 +0900

    댓글 삭제시 에러 수정

--- root/commentDelete.php
+++ root/commentDelete.php
@@ -1,10 +1,6 @@
 <?php
     include "../DBConnect.php";
 
-    ?><script type="text/javascript">
-        alert('1');
-    </script><?php
-
     $rno = $_POST['rno'];
 
     //comment 테이블에서 idxNum가 rno변수에 저장된 값을 찾음
@@ -13,36 +9,25 @@
 
     $bno = $_POST['b_no'];
 
-    ?><script type="text/javascript">
-        alert('2');
-    </script><?php
-
     //bulletinBoard 테이블에서 idxNum가 bno변수에 저장된 값을 찾음
-    $sql_bulletinBoard = mq("select * from board where idxNum='".$bno."'");
+    $sql_bulletinBoard = mq("select * from bulletinBoard where idxNum='".$bno."'");
     $board = $sql_bulletinBoard->fetch_array();
 
     $pwk = $_POST['password'];
     $bpw = $comment['password'];
 
-    ?><script type="text/javascript">
-        alert('3');
-    </script><?php
+    if(password_verify($pwk, $bpw)) {
+        $sql = mq("delete from comment where idxNum='".$rno."'"); ?>
 
-if(password_verify($pwk, $bpw)) {
-    ?><script type="text/javascript">
-        alert('3');
-    </script><?php
-
-    $sql = mq("delete from comment where idxNum='".$rno."'"); ?>
-    <script type="text/javascript">
-        alert('댓글이 삭제되었습니다.');
-        location.replace("./bulletinBoardRead.php?idxNum=<?php echo $board["idxNum"];?>");
-    </script>
-    <?php
-} else{ ?>
-    <script type="text/javascript">
-        alert('비밀번호가 틀립니다'); history.back();
-    </script>
-<?php } ?>
+        <script type="text/javascript">
+            alert('댓글이 삭제되었습니다.');
+            location.replace("./bulletinBoardRead.php?idxNum=<?php echo $board["idxNum"];?>");
+        </script>
+        <?php
+    } else{ ?>
+        <script type="text/javascript">
+            alert('비밀번호가 틀립니다'); history.back();
+        </script>
+    <?php } ?>
 
 
