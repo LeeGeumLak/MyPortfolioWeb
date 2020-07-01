f67e51e4e1e3b339508b09aa031c5ef5165a0cd5
LeeGeumLak <ekdma4105@naver.com>
2020-07-01 14:06:43 +0900

    댓글 삭제시 에러 수정

--- root/commentDelete.php
+++ root/commentDelete.php
@@ -15,14 +15,15 @@
     $pwk = $_POST['password'];
     $bpw = $comment['password'];
 
-    if(password_verify($pwk, $bpw)) {
-        $sql = mq("delete from comment where idxNum='".$rno."'"); ?>
-        <script type="text/javascript">
-            //alert('댓글이 삭제되었습니다.');
-            location.replace("./bulletinBoardRead.php?idxNum=<?php echo $bno; ?>");
-        </script>
-        <?php
-    } else { ?>
+if(password_verify($pwk, $bpw)) {
+    $sql = mq("delete from comment where idxNum='".$rno."'"); ?>
+    <!--<script type="text/javascript">
+        alert('댓글이 삭제되었습니다.');
+        location.replace("./bulletinBoardRead.php?idxNum=<?php /*echo $board["idxNum"];*/?>");
+    </script>-->
+
+    <?php
+} else{ ?>
     <script type="text/javascript">
         alert('비밀번호가 틀립니다'); history.back();
     </script>
