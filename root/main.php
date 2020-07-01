d80e97588315492c0251358a09b7a75d99435f55
LeeGeumLak <ekdma4105@naver.com>
2020-07-01 16:58:39 +0900

    메인 페이지 텍스트 수정

--- root/main.php
+++ root/main.php
@@ -63,7 +63,8 @@
         </div>-->
 
         <div class="typer" id="h">
-            <h1 class="typer-text">환영합니다.<br>개발자 이금락의 포트폴리오입니다.</h1>
+            <h1 style="text-align: center; font-family: Noto Sans KR,sans-serif; border-right: .1em solid #fff; font-size: 4vw; white-space: pre; line-height: 1; letter-spacing: .1rem; padding-top: .2em;">환영합니다.<br><br></h1>
+            <h1 class="typer-text" style="text-align: center">개발자 이금락의 포트폴리오입니다.</h1>
         </div>
 
         <?php
