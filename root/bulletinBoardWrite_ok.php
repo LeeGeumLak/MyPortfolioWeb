5c9a44f48c41c346b98f4cd51eb53b113ca946b0
LeeGeumLak <ekdma4105@naver.com>
2020-07-02 01:25:41 +0900

    게시판 글 작성자 입력시, 예외처리

--- root/bulletinBoardWrite_ok.php
+++ root/bulletinBoardWrite_ok.php
@@ -28,24 +28,33 @@
 
     // client 단에서 입력한 작성자 명이 db 에 저장된 닉네임에 있으면, 다시 입력
     $sql = mq("select nickName from userInfo");
+    $writePossible = 'true';
     while($nickName = $sql->fetch_array()) {
-        if($username == 'admin') {
-            echo "<script> alert('admin 은 작성자명으로 사용할 수 없습니다. 다른 작성자명으로 입력해주세요.'); </script>";
+        if( ($username == 'admin') || ($username == '어드민') || ($username == '운영자') ) {
+            echo "<script> alert('$username 은 작성자명으로 사용할 수 없습니다. 다른 작성자명으로 입력해주세요.'); </script>";
+            $writePossible = 'false';
+            break;
         }
-        if($username == $nickName['nickName']) {
+        else if($username == $nickName['nickName']) {
             echo "<script> alert('$username 은 회원의 닉네임입니다. 다른 작성자명으로 입력해주세요.'); </script>";
+            $writePossible = 'false';
+            break;
         }
     }
 
-    if($username && $userpassword && $title && $content){
-        $sql = mq("insert into bulletinBoard (name, password, title, content, writeDate, lock_post, file) values('".$username."','".$userpassword."','".$title."','".$content."','".$date."','".$lock_post."','".$o_name."')");
+    if($writePossible == 'true') {
+        if ($username && $userpassword && $title && $content) {
+            $sql = mq("insert into bulletinBoard (name, password, title, content, writeDate, lock_post, file) values('" . $username . "','" . $userpassword . "','" . $title . "','" . $content . "','" . $date . "','" . $lock_post . "','" . $o_name . "')");
 
-        echo "<script>
+            echo "<script>
         //alert('글쓰기 완료되었습니다.');
         location.href='./bulletinBoard.php';</script>";
-    } else{
-        echo "<script>
+        } else {
+            echo "<script>
         alert('오류로 인해 글쓰기에 실패했습니다. 다시 시도해주세요.');
         history.back();</script>";
+        }
+    } else {
+        // do nothing
     }
 ?>
