70907daf7e2ba60654b8b10d684563dc80c10ae7
LeeGeumLak <ekdma4105@naver.com>
2020-07-06 15:44:39 +0900

    add: adminGraph.php 레이아웃 작업 완성

--- root/adminGraph.php
+++ root/adminGraph.php
@@ -1,141 +1,253 @@
 <?php
-function pre_dump($array){
-    echo "<pre>";
-    echo var_dump($array);
-    echo "</pre>";
-}
+    function pre_dump($array){
+        echo "<pre>";
+        echo var_dump($array);
+        echo "</pre>";
+    }
 
-//페이징 관련 변수들 지정
-$page_num = ($_GET['page']) ? $_GET['page'] : 1; //page : default = 1
-$list = ($_GET['list']) ? $_GET['list'] : 10; //page : default = 10
+    session_start();
+    if(isset($_SESSION['userId'])){
+        if(!($_SESSION['userId'] == 'admin@naver.com')) {?>
+            <script type="text/javascript">
+                alert("관리자만 접근할 수 있습니다.");
+                location.replace("./main.php");
+            </script> <?php
+        } else {
+            include '../DBConnect.php';
+        }
+    }
+    else {?>
+        <script type="text/javascript">
+            alert("관리자만 접근할 수 있습니다.");
+            location.replace("./main.php");
+        </script> <?php
+    }
 
-
-$block_page_num_list = 10; //블럭에 나타낼 페이지 번호 갯수
-$block = ceil($page_num/$block_page_num_list); //현재 리스트의 블럭 구하기
-
-
-$block_start_page = ( ($block - 1) * $block_page_num_list ) + 1; //현재 블럭에서 시작페이지 번호
-$block_end_page = $block_start_page + $block_page_num_list - 1; //현재 블럭에서 마지막 페이지 번호
+    //랜덤 데이터 입력
+    //for($i=0; $i<100; $i++){
+    //
+    //    $random = rand(0,12);
+    //
+    //    $sql = "INSERT INTO userLog (user_id, date, ip, previous_url, country, current_url)
+    //    VALUES ('thdalsehf@naver.com', date_add(now(), interval +$random hour), '125.188.15.156', 'https://wowtravel.tk/index.php', 'KR', 'https://wowtravel.tk/main.php')";
+    //    $conn->query($sql);
+    //
+    //    $sql = "INSERT INTO userLog (user_id, date, ip, previous_url, country, current_url)
+    //	VALUES ('msbe@naver.com', date_add(now(), interval +$random hour), '125.188.15.156', 'https://wowtravel.tk/index.php', 'KR', 'https://wowtravel.tk/main.php')";
+    //    $conn->query($sql);
+    //
+    //    $sql = "INSERT INTO userLog (user_id, date, ip, previous_url, country, current_url)
+    //	VALUES ('msbe2@naver.com', date_add(now(), interval +$random hour), '125.188.15.156', 'https://wowtravel.tk/index.php', 'KR', 'https://wowtravel.tk/main.php')";
+    //    $conn->query($sql);
+    //
+    //    $sql = "INSERT INTO userLog (user_id, date, ip, previous_url, country, current_url)
+    //	VALUES ('msbe3@naver.com', date_add(now(), interval +$random hour), '125.188.15.156', 'https://wowtravel.tk/index.php', 'KR', 'https://wowtravel.tk/main.php')";
+    //    $conn->query($sql);
+    //
+    //}
 ?>
 
-<div class="container_big" style="height: 100%; margin:20px;">
-    <nav class="side_bar fl">
-        <div class="sign">네비게이션</div>
-        <ul>
-            <li>
-                <a class="on" href="./adminMain.php">표</a>
-            </li>
-            <li>
-                <a href="./adminGraph.php">그래프</a>
-            </li>
-        </ul>
-    </nav>
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php include './header.php'?>
+        <link rel="stylesheet" href="../css/graph.css" />
 
-    <!-- 유저로그 테이블 -->
-    <div class="user_log fl">
-        <table class="table">
-            <colgroup>
-                <col width="8%">
-                <col width="10%">
-                <col width="10%">
-                <col width="15%">
-                <col width="15%">
-                <col width="15%">
-                <col width="10%">
-            </colgroup>
+        <!--    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>-->
+        <script src="https://code.highcharts.com/highcharts.js"></script>
+        <script src="https://code.highcharts.com/modules/data.js"></script>
+        <script src="https://code.highcharts.com/modules/series-label.js"></script>
+        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+        <script src="https://code.highcharts.com/modules/export-data.js"></script>
+        <script src="https://code.highcharts.com/modules/accessibility.js"></script>
 
-            <thead>
-            <tr>
-                <th style="text-align: center;">번호</th>
-                <!--                            <th style="text-align: center;">아이디</th>-->
-                <th style="text-align: center;">IP</th>
-                <th style="text-align: center;">국가</th>
-                <th style="text-align: center;">이전경로</th>
-                <th style="text-align: center;">들어온경로</th>
-                <th style="text-align: center;">날짜</th>
-            </tr>
-            </thead>
+        <!-- Additional files for the Highslide popup effect -->
+        <script src="https://www.highcharts.com/media/com_demo/js/highslide-full.min.js"></script>
+        <script src="https://www.highcharts.com/media/com_demo/js/highslide.config.js" charset="utf-8"></script>
+        <link rel="stylesheet" type="text/css" href="https://www.highcharts.com/media/com_demo/css/highslide.css" />
+    </head>
 
-            <tbody style="text-align: center;">
-            <!-- userLog 목록 가져오기 -->
-            <?php
-            include '../DBConnect.php';
+    <body style="background-image: none">
+        <!-- 상단 부분 -->
+        <?php include './topPart.php'?>
 
-            //게시글 시작위치
-            $limit = ($page_num-1)*$list;
+        <div class="container_big" style="height: 100%; margin:20px;  margin-top: 100px"">
+            <nav class="side_bar fl">
+                <div class="sign">네비게이션</div>
+                <ul>
+                    <li>
+                        <a href="./adminMain.php">표</a>
+                    </li>
+                    <li>
+                        <a class="on" href="./adminGraph.php">그래프</a>
+                    </li>
+                </ul>
+            </nav>
 
-            $sql = "select * from userLog order by idxNum desc limit $limit,$list";
-            $result = $db->query($sql);
-            while($row = $result->fetch_assoc()) {
-                $datetime = explode(' ', $row['accessDate']);
-                $date = $datetime[0];
-                $time = $datetime[1];
-                if($date == Date('Y-m-d')) {
-                    $row['accessDate'] = $time;
-                } else {
-                    $row['accessDate'] = $date;
-                }
-                ?>
-                <tr class="freeBoardHover" onclick="goFreeBoardView(<?php echo $row['idxNum'] ?>)">
-                    <td><?php echo $row['idxNum']?></td>
-                    <!-- <td><div style="max-height:17px;overflow: hidden"; ><?php /*echo $row['userId']*/ ?></div></td> -->
-                    <td><div style="max-height:17px;overflow: hidden"; ><?php echo $row['ip']?></div></td>
-                    <td><?php echo $row['country']?></td>
-                    <td><?php echo $row['previousUrl']?></td>
-                    <td><?php echo $row['currentUrl']?></td>
-                    <td><?php echo $row['accessDate']?></td>
-                </tr>
-                <?php
-            } ?>
-            </tbody>
-        </table>
+            <!-- 유저로그 차트 -->
+            <div class="admin_content_container fl">
+                <div>
+                    <h3 style="display: inline-block;">연도별, 월별, 시간별 선택 : </h3>
+                    <select id="graph_select" style="display: inline-block;">
+                        <option value="">그래프 선택</option>
+                        <option value="year">연도별</option>
+                        <option value="month">월별</option>
+                        <option value="hour">시간별</option>
+                    </select>
+                </div>
+                <figure class="highcharts-figure" style="max-width: 100%;">
+                    <div id="container"></div>
+                </figure>
+            </div>
+        </div>
 
-        <!-- 페이징 처리 -->
-        <nav aria-label="Page navigation" style="text-align: center;">
-            <ul class="pagination">
-                <?php
-                //DB에서 총 데이터 개수 가져오기
-                $sql = "SELECT COUNT(*) FROM userLog";
-                $result = $db->query($sql);
-                $row = $result->fetch_assoc();
-                $total_count = $row["COUNT(*)"];
+        <script type="text/javascript">
+            let title;
+            let subtitle;
+            let categories = [];
+            let data =[];
 
-                $total_page =  ceil($total_count/$list); //총 페이지 수
+            $(document).on('ready', function(e){
+                $("#graph_select").on("change",function(){
+                    let select_value = $("#graph_select").val();
+                    if(select_value == "year"){
+                        $.ajax({
+                            type: "POST",
+                            url : "./userYearlyVisit.php",
+                            data: {},
+                            dataType:"json",
+                            success : function(ajax_data, status, xhr) {
+                                //초기화
+                                categories = [];
+                                data =[];
 
-                if ($block_end_page > $total_page) {
-                    $block_end_page = $total_page;
-                }
+                                title ="연별 방문자 수";
+                                subtitle = "";
+                                for(let i=0; i<ajax_data.length; i++){
+                                    categories[i] = ajax_data[i].year+"년";
+                                    data[i] = ajax_data[i].count*1;
+                                }
+                                //그래프 리로드
+                                graph_reload(title, subtitle, categories, data);
 
-                $before = $block_start_page-1;
-                //페이징 block단위로 뒤로가기
-                if($block != 1){?>
-                    <li><a href="./adminMain.php?page=<?=$before?>">이전</a></li>
-                    <?php
-                }
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                console.log(jqXHR.responseText);
+                            }
+                        });
+                    }else if(select_value == "month"){
+                        $.ajax({
+                            type: "POST",
+                            url : "./userMonthlyVisit.php",
+                            data: {},
+                            dataType:"json",
+                            success : function(ajax_data, status, xhr) {
+                                //초기화
+                                categories = [];
+                                data =[];
 
-                //페이징 숫자부분
-                for($i = $block_start_page; $i <=$block_end_page; $i++) {
-                    if($page_num == $i){?>
-                        <li><a style="background: silver;"><?=$i?></a></li>
-                        <?php
-                    } else{?>
-                        <li><a href="./adminMain.php?page=<?=$i?>"><?=$i?></a></li>
-                        <?php
+                                title ="월별 방문자 수";
+                                subtitle = "올해기준";
+                                categories = ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'];
+                                for(let i=0; i<ajax_data.length; i++){
+                                    data[i] = ajax_data[i].count*1;
+                                }
+                                //그래프 리로드
+                                console.log("data:",data);
+                                graph_reload(title, subtitle, categories, data);
+
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                console.log(jqXHR.responseText);
+                            }
+                        });
+
+                    }else if(select_value == "hour"){
+                        $.ajax({
+                            type: "POST",
+                            url : "./userHourlyVisit.php",
+                            data: {},
+                            dataType:"json",
+                            success : function(ajax_data, status, xhr) {
+                                //초기화
+                                categories = [];
+                                data =[];
+
+                                title ="시간별 방문자 수";
+                                subtitle = "오늘기준";
+                                for(let i=0; i<25; i++){
+                                    categories[i] = i+"시";
+                                }
+                                for(let i=0; i<ajax_data.length; i++){
+                                    data[i] = ajax_data[i].count*1;
+                                }
+                                //그래프 리로드
+                                console.log("data:",data);
+                                graph_reload(title, subtitle, categories, data);
+
+                            },
+                            error: function(jqXHR, textStatus, errorThrown) {
+                                console.log(jqXHR.responseText);
+                            }
+                        });
                     }
-                }
+                });
 
-                $next = $block_end_page+1;
-                if($next>=$total_page){
-                    $next = $total_page;
-                }
-                $total_block = ceil($total_page/$block_page_num_list);
-                //페이징 block단위로 다음으로
-                if($block != $total_block){?>
-                    <li><a href="./adminMain.php?page=<?=$next?>">다음</a></li>
-                    <?php
-                }
-                ?>
-            </ul>
-        </nav>
-    </div>
-</div>
+                //처음에 연도별 선택
+                $("#graph_select").val('year').trigger('change');
+
+            });
+
+            //그래프 리로드
+            function graph_reload(title, subtitle, categories, data){
+                Highcharts.chart('container', {
+                    title: {
+                        text: title
+                    },
+                    subtitle: {
+                        text: subtitle
+                    },
+                    yAxis: {
+                        title: {
+                            text: '방문자 수 [명]'
+                        }
+                    },
+                    xAxis: {
+                        categories: categories
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
+                    },
+                    plotOptions: {
+                        line: {
+                            dataLabels: {
+                                enabled: true
+                            },
+                            enableMouseTracking: false
+                        }
+                    },
+                    series: [{
+                        name: '방문자 수',
+                        data: data
+                    }],
+                    responsive: {
+                        rules: [{
+                            condition: {
+                                maxWidth: 500
+                            },
+                            chartOptions: {
+                                legend: {
+                                    layout: 'horizontal',
+                                    align: 'center',
+                                    verticalAlign: 'bottom'
+                                }
+                            }
+                        }]
+                    }
+                });
+            }
+        </script>
+    </body>
+</html>
