cba83f40ff68a43d3420c5bc65d01970f674e8d9
LeeGeumLak <ekdma4105@naver.com>
2020-07-06 16:07:19 +0900

    add: userLog.php 접속자의 id, ip, 이전 url, 현재 url, 국가, 접속시간을 받아와 db에 저장하는 코드 추가

--- root/userLog.php
+++ root/userLog.php
@@ -1,1 +1,42 @@
 <?php
+    //쿠키가 존재하지 않는다면
+    if(!isset($_COOKIE['user_log_cookie'])){
+        //쿠키생성
+        setcookie('user_log_cookie', true, time() + 60*30);
+
+        //user_log정보 insert
+        include '../DBConnect.php';
+
+        //Post로 받은 데이터 가져오기
+        $userId = $_SESSION['userId'];
+        $ip = $_SERVER["REMOTE_ADDR"];
+        $previousUrl = $_SERVER['HTTP_REFERER'];
+
+        //현재 url 가져오기
+        $httpHost = $_SERVER['HTTP_HOST'];
+        $requestUri = $_SERVER['REQUEST_URI'];
+        $currentUrl = 'https://' . $httpHost . $requestUri;
+
+        //들어온 ip의 국가 가져오기
+        $key = "2020070616035390754922";
+        $data_format = "json";
+        $url = "http://whois.kisa.or.kr/openapi/ipascc.jsp?query=$ip&key=$key&answer=$data_format";
+
+        $ch = curl_init();                                              //curl 초기화
+        curl_setopt($ch, CURLOPT_URL, $url);                      //URL 지정하기
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     //요청 결과를 문자열로 반환
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);       //connection timeout 10초
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    //원격 서버의 인증서가 유효한지 검사 안함
+
+        $data = curl_exec($ch);
+
+        $decodeJsonData = json_decode($data, true);
+        $country = $decodeJsonData["whois"]["countryCode"];
+        curl_close($ch);
+
+        $sql = "INSERT INTO userLog (userId, ip, country, previousUrl, currentUrl, accessDate) 
+                VALUES ('$userId', '$ip', '$country', '$previousUrl', '$currentUrl', now())";
+
+        $db->query($sql);
+    }
+?>
