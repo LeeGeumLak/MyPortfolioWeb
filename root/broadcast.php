bc16bcfd636b231170450baadb3550b3682e5457
LeeGeumLak <ekdma4105@naver.com>
2020-07-07 14:26:53 +0900

    modify: broadcast.php, socket.js 채팅 기능 구현을 위한 로직 수정

--- root/broadcast.php
+++ root/broadcast.php
@@ -10,7 +10,10 @@
         echo("<script>location.href='./signIn.php';</script>");
     //로그인 세션 있을때, 방송 페이지 접근
     } else {
-        // do nothing
+        // 접근한 유저의 닉네임 가져오기
+        $sql = mq("select nickName from userInfo where userId='".$_SESSION['userId']."'");
+        $nickName = $sql->fetch_array();
+        // 유저 닉네임 == $nickName['nickName']
     }
 ?>
 
@@ -19,44 +22,7 @@
     <head>
         <?php include './header.php'?>
 
-        <link href="https://unpkg.com/video.js/dist/video-js.css" rel="stylesheet">
-        <script src="node_modules/socket.io-client/dist/socket.io.js"></script>
-        <script src="https://unpkg.com/video.js/dist/video.js"></script>
-        <script src="https://unpkg.com/videojs-flash/dist/videojs-flash.js"></script>
-
-        <style>
-            .chat{position:relative;border:1px solid #ddd;height:500px;}
-            .chat form{position:absolute;bottom:0;left:0;width:100%;padding:10px;box-sizing:border-box;}
-            .chat form input[type="text"]{border:0;padding:0 15px;height:35px;line-height:35px;width:calc(100% - 140px);box-sizing:border-box;outline:none;border-bottom:1px solid #ddd;}
-            .chat form button{border:1px solid #ddd;background-color:#fff;color:#333;width:100px;height:35px;line-height:33px;margin-left:20px;}
-            #messages{height:calc(500px - 58px);overflow-y:scroll;}
-            #messages li{}
-        </style>
-
-        <script src="../node_modules/socket.io/lib/socket.js"></script>
-        <script>
-            var socket = io();
-        </script>
-
-        <script src="https://code.jquery.com/jquery-1.11.1.js"></script>
-        <script>
-            $(function () {
-                var socket = io();
-
-                $('form').submit(function(e){
-                    e.preventDefault(); // prevents page reloading ( 페이지 새로고침 막기 )
-                    socket.emit('chat message', $('#m').val()); // "chat message"라는 이벤트 생성
-                    $('#m').val(''); // 보낸 메세지 초기화
-                    return false;
-                });
-
-                socket.on('chat message', function(msg){
-                    var tag = "<li>" + msg + "</li>";
-                    $('#messages').append(tag);
-                });
-            });
-        </script>
-
+        <script src="../node_modules/socket.io-client/dist/socket.io.js"></script>
     </head>
 
     <body>
@@ -70,11 +36,20 @@
                     BROADCAST
                 </div>
 
-                <div class="chat">
-                    <ul id="messages"></ul>
-                    <form action="">
-                        <input type="text" id="m" autocomplete="off" placeholder="입력해주세요 :D" /><button>전송</button>
-                    </form>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-auto offset-2">
+                            <input type="text" id="name"><?php echo $nickName['nickName'] ?><br>
+                            <textarea rows="20" cols="40" id="chatRoom"></textarea><br>
+                            <input type="text" class="col-9" id="chatInput">
+                            <input type="button" class="btn btn-sm btn-info" value="보내기" onclick="sendMsg()">
+                        </div>
+                        <!-- 접속자 명단 -->
+                        <div class="col col-lg-2">
+                            <label for="userlist">접속자 목록</label>
+                            <div id="userlist" class="bg-white" style="height: 400px; border:1px solid black; overflow: auto"></div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
@@ -89,10 +64,49 @@
             }
         ?>
 
-
-
         <?php include './footer.php'?>
 
         <script src="../js/myPortfolioWeb.js"></script>
+
+        <script type="text/javascript">
+            // 소켓을 연결
+            const socket = io.connect(':3000');
+
+            // 서버에서 전달한 데이터가 있을 때 실행되는 이벤트 리스너
+            //receiveMessage 이벤트를 전달받으면 msg.comment를 클라이언트 콘솔에 출력+chatRoom(textarea)에 누적 출럭
+            socket.on('receiveMessage', function (msg) {
+                console.log(msg.comment)
+                $('#chatRoom').append(msg.comment);
+                // TODO: 스크롤 자동으로 내려가도록 하기
+                // $('#chatRoom').scrollTop($('#chatRoom')[0].scrollHeight);
+            });
+            // 새로 접속한 유저에게 user+n 의 새 이름을 배정한다
+            socket.on('changeName', function(name){
+                let userName = name;
+                console.log('name: ', userName);
+                $('#name').val(userName);
+
+            });
+
+            let userList = [];
+            socket.on('update', function(users){
+                userList = users;
+                console.log('userList: ', userList);
+                // 업데이트를 하기 전 중첩을 방지하기 위해서 이미 출력되어있던 텍스트를 지운다
+                $('#userlist').empty();
+                for(let i=0; i < userList.length; i++){
+                    $('#userlist').append(userList[i].name +'</br>')
+                }
+            });
+
+            // 보내기 버튼의 클릭이벤트. 소켓의 데이터를 보낸다.
+            // comment라는 변수에 input태그(채팅입력창)값을 담아서 전송한 뒤 입력창 내용은 지운다
+            function sendMsg() {
+                socket.emit('sendMessage', { comment: $('#chatInput').val() });
+                console.log(comment);
+                $('#chatInput').empty();
+                $('#chatInput').focus();
+            }
+        </script>
     </body>
 </html>
