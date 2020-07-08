79df32fb5c0d6fe49083308977ad8c1b5f3d141e
LeeGeumLak <ekdma4105@naver.com>
2020-07-08 16:17:33 +0900

    modify: main.php 유저로그 남기는 파일 include 하는 코드 주석해제

--- root/main.php
+++ root/main.php
@@ -13,7 +13,7 @@
         <?php include './topPart.php'?>
 
         <!--  유저로그를 db에 저장하는 파일  -->
-        <? include './userLog.php'; ?>
+        <? include './userLog.php' ?>
 
         <!--  메인화면에 표시될 텍스트 타이퍼 -->
         <div class="typer" id="h">
@@ -36,57 +36,6 @@
 
         <!-- 메인 js 파일 스크립트 추가 -->
         <script src="../js/myPortfolioWeb.js"></script>
-
-        <!--<script type="text/javascript">
-            var imageSlideIndex = 1;
-            showImageSlides(imageSlideIndex);
-
-            function imageSlideTimer() {
-                plusImageSlides(1);
-            }
-
-            var imageTimer = setInterval(imageSlideTimer, 3000);
-
-            function plusImageSlides(n) {
-                clearInterval(imageTimer);
-                imageTimer = setInterval(imageSlideTimer, 3000);
-
-                showImageSlides(imageSlideIndex += n);
-            }
-
-            function currentImageSlide(n){
-                clearInterval(imageTimer);
-                imageTimer = setInterval(imageSlideTimer, 3000);
-
-                showImageSlides(imageSlideIndex = n);
-            }
-
-            function showImageSlides(n) {
-                var i;
-                var slides = document.getElementsByClassName('image-slide');
-                var slide = document.getElementsByClassName('slide');
-                if (n > slides.length) {imageSlideIndex = 1}
-                if (n < 1) {imageSlideIndex = slides.length}
-                for (i = 0; i < slides.length; i++) {
-                    slides[i].style.display = 'none';
-                }
-                for (i = 0; i < slide.length; i++) {
-                    slide[i].className = slide[i].className.replace(' active', '');
-                }
-                slides[imageSlideIndex-1].style.display = 'block';
-                slide[imageSlideIndex-1].className += ' active';
-            }
-
-            document.getElementById('imagePrev').addEventListener('click', plusImageSlides.bind(null,-1));
-            document.getElementById('imageNext').addEventListener('click', plusImageSlides.bind(null,1));
-
-            document.getElementById('firstSlide').addEventListener('click', currentImageSlide.bind(null,1));
-            document.getElementById('secondSlide').addEventListener('click', currentImageSlide.bind(null,2));
-            document.getElementById('thirdSlide').addEventListener('click', currentImageSlide.bind(null,3));
-            document.getElementById('forthSlide').addEventListener('click', currentImageSlide.bind(null,4));
-            document.getElementById('fifthSlide').addEventListener('click', currentImageSlide.bind(null,5));
-            document.getElementById('sixthSlide').addEventListener('click', currentImageSlide.bind(null,6));
-        </script>-->
 
         <script type="text/javascript">
             let typingBool = false;
