d71e391f306eb300b20bbb963b3a276ea72a5918
LeeGeumLak <ekdma4105@naver.com>
2020-07-08 16:37:14 +0900

    add: userLog.php 버그 지점 알아보기위해 콘솔로그 코드추가

--- root/userLog.php
+++ root/userLog.php
@@ -7,8 +7,6 @@
         //userLog정보 insert
         include '../DBConnect.php';
 
-        echo "<script>console.log( '1' );</script>";
-
         //Post로 받은 데이터 가져오기
         session_start();
         if(isset($_SESSION['userId'])) {
@@ -17,49 +15,57 @@
             $userId = '';
         }
 
-        echo "<script>console.log( '2' );</script>";
-
         $ip = $_SERVER['REMOTE_ADDR'];
         $previousUrl = $_SERVER['HTTP_REFERER'];
-
-        echo "<script>console.log( '3' );</script>";
 
         //현재 url 가져오기
         $httpHost = $_SERVER['HTTP_HOST'];
         $requestUri = $_SERVER['REQUEST_URI'];
         $currentUrl = 'https://' . $httpHost . $requestUri;
 
-        echo "<script>console.log( '4' );</script>";
-
         //들어온 ip의 국가 가져오기
         $key = "2020070616035390754922";
         $data_format = "json";
         $url = "http://whois.kisa.or.kr/openapi/ipascc.jsp?query=".$ip."&key=".$key."&answer=".$data_format."";
 
-        echo "<script>console.log( '5' );</script>";
+        echo "<script>console.log( '1' );</script>";
 
         $ch = curl_init();                                              //curl 초기화
+echo "<script>console.log( '2' );</script>";
+
         curl_setopt($ch, CURLOPT_URL, $url);                      //URL 지정하기
+echo "<script>console.log( '3' );</script>";
+
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     //요청 결과를 문자열로 반환
+echo "<script>console.log( '4' );</script>";
+
         curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);       //connection timeout 10초
+echo "<script>console.log( '5' );</script>";
+
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);    //원격 서버의 인증서가 유효한지 검사 안함
+echo "<script>console.log( '6' );</script>";
 
         $data = curl_exec($ch);
+echo "<script>console.log( '7' );</script>";
 
         $decodeJsonData = json_decode($data, true);
-        $country = $decodeJsonData['whois']['countryCode'];
+echo "<script>console.log( '8' );</script>";
+
+$country = $decodeJsonData['whois']['countryCode'];
+echo "<script>console.log( '9' );</script>";
+
         curl_close($ch);
 
-        echo "<script>console.log( '6' );</script>";
+        echo "<script>console.log( '0' );</script>";
 
         // auto_increment 값 초기화
         $sql_autoIncrement = mq("alter table userLog auto_increment =1");
 
-        echo "<script>console.log( '7' );</script>";
+        echo "<script>console.log( '1' );</script>";
 
         $sql = mq("INSERT INTO userLog (userId, ip, country, previousUrl, currentUrl, accessDate) 
                 VALUES (".$userId.", ".$ip.", ".$country.", ".$previousUrl.", ".$currentUrl.", now())");
 
-        echo "<script>console.log( '8' );</script>";
+        echo "<script>console.log( '2' );</script>";
     //}
 ?>
